8b5db2697b51fdcc75c02fc72a8425470630866a
Richard <r-duerden@live.co.uk>
2025-12-01 17:09:06 +0000

    still trying to fix static

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -141,9 +141,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
