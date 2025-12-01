d90baf246d0d9c3eebb58a652127c8dcf97a709f
Richard <r-duerden@live.co.uk>
2025-12-01 16:03:19 +0000

    secret key issue sorted

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os 
 os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_kjpEYzAeT72m@ep-gentle-boat-agkpqa3s.c-2.eu-central-1.aws.neon.tech/donut_musky_wok_658654')
 
-os.environ.setdefault('SECRET_KEY', 'django-insecure-$e@c2rbzppb-3w&^25n8q=rzrkm4(^@@^$j3z23f6mk3m880wr')
+os.environ.setdefault('SECRET_KEY', '63f4945d921d599f27ae4fdf5bada3f1')
