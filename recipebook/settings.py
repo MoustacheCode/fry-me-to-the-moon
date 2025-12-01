737067ef35020c038ea8abd3750ce06b8c0010c3
Richard <r-duerden@live.co.uk>
2025-12-01 17:11:19 +0000

    here we go again

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -141,7 +141,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
